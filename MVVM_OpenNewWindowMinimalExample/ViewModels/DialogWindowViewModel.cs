34fbec20c70602fe8dce62e8c5fcaef824410930
FedorrV <fdudnikov@gmail.com>
2020-05-01 14:57:24 +0400

    init

---
+++ MVVM_OpenNewWindowMinimalExample/ViewModels/DialogWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace MVVM_OpenNewWindowMinimalExample.ViewModels {
+    class DialogWindowViewModel:BasicViewModel {
+        private string textValue;
+        public string TextValue
+        {
+            get { return textValue; }
+            set
+            {
+                textValue = value;
+                TextBlock = textValue;
+                OnPropertyChanged("TextValue");
+            }
+        }
+
+        private string textBlock;
+        public string TextBlock
+        {
+            get { return textBlock; }
+            set
+            {
+                textBlock = value;
+                OnPropertyChanged("TextBlock");
+            }
+        }
+    }
+}
